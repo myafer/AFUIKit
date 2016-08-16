660a387039d8ca03e621f9b5c575a1bb90c4571e
myafer <791143125@qq.com>
2016-08-16 17:02:52 +0800

    Add UITextView
    Add UITextView

---
+++ AFUIKit/UITextView+AFExtension.swift
@@ -0,0 +1,16 @@
+//
+//  UITextView+AFExtension.swift
+//  AFUIKitExample
+//
+//  Created by 口贷网 on 16/8/16.
+//  Copyright © 2016年 Afer. All rights reserved.
+//
+
+
+
+import UIKit
+
+extension UITextView {
+
+
+}
